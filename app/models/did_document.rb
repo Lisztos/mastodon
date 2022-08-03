f82f2b8889affd902c30f05a912886bfb9d47747
Lisztos <isaias.liszt@gmail.com>
2022-08-03 18:22:56 +0000

    chore: added did document parser

--- app/models/did_document.rb
+++ app/models/did_document.rb
@@ -1,12 +1,14 @@
 class DidDocument
 
+  DIDCOMM_KEY_SELECTOR = "#delegate-1".freeze
+
   attr_reader :id,
               :publicKey,
               :authentication,
               :assertionMethod,
               :capabilityDelegation,
               :capabilityInvocation,
-              :keyAgreement,
+              :verificationMethod,
               :service
 
   def initialize(attributes)
@@ -14,10 +16,38 @@ class DidDocument
     @publicKey = attributes['publicKey']
     @authentication = attributes['authentication']
     @assertionMethod = attributes['assertionMethod']
-    @capabilityDelegation = attributes['capabilityDelegation']
-    @capabilityInvocation = attributes['capabilityInvocation']
-    @keyAgreement = attributes['keyAgreement']
+    @verificationMethod = attributes['verificationMethod']
     @service = attributes['service']
+  end
+
+  def rsa_didcomm_key
+    pem = dig_didcomm_key['publicKeyPem']
+    key = clean_pem(pem)
+    return OpenSSL::PKey::RSA.new(key) 
+  end
+
+  def activitypub_enpoint
+    service[0]['serviceEndpoint']
+  end
+
+  private
+
+  def dig_didcomm_key
+    key = nil
+    verificationMethod.each do |k|
+      if k['id'].include?(DIDCOMM_KEY_SELECTOR)
+        key = k
+      end
+    end
+    key
+  end
+
+  def clean_pem(pem)
+    p = pem.gsub(' ', '')
+    p = p.gsub('ENDPUBLICKEY', 'END PUBLIC KEY')
+    p = p.gsub('BEGINPUBLICKEY', 'BEGIN PUBLIC KEY')
+    p = p.gsub('ENDPRIVATEKEY', 'END PRIVATE KEY')
+    p = p.gsub('BEGINPRIVATEKEY', 'BEGIN PRIVATE KEY')
   end
 
 end
