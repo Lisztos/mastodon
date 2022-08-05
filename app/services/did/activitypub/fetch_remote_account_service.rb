bdfca65d85c5be66ff68585f18ac88535a61d08b
Lisztos <isaias.liszt@gmail.com>
2022-08-05 15:03:10 +0000

    chore: finalized search class for did users

--- app/services/did/activitypub/fetch_remote_account_service.rb
+++ app/services/did/activitypub/fetch_remote_account_service.rb
@@ -6,31 +6,32 @@ class Did::ActivityPub::FetchRemoteAccountService < BaseService
 
   SUPPORTED_TYPES = %w(Application Group Organization Person Service).freeze
 
-  # Does a WebFinger roundtrip on each call, unless `only_key` is true
-  def call(uri, id: true, prefetched_body: nil, break_on_redirect: false, only_key: false)
+  def call(uri, id: true, prefetched_body: nil, break_on_redirect: false)
 
     return ActivityPub::TagManager.instance.uri_to_resource(uri, Account) if ActivityPub::TagManager.instance.local_uri?(uri)
 
     @json = if prefetched_body.nil?
-              fetch_resource(uri, id)
-            else
-              body_to_json(prefetched_body, compare_id: id ? uri : nil)
+            fetch_resource(uri, id)
+          else
+            body_to_json(prefetched_body, compare_id: id ? uri : nil)
             end
 
     return if !supported_context? || !expected_type? || (break_on_redirect && @json['movedTo'].present?)
-
+    
     @uri      = @json['id']
     @username = @json['preferredUsername']
-
-    return unless only_key 
+    @domain   = parse_domain
   
-    Rails.logger.info "ActivityPub: Process Account service with: Username#{@username}, domain: nil, @json: #{@json}"
-    ActivityPub::ProcessAccountService.new.call(@username, nil, @json, only_key: only_key, verified_webfinger: !only_key)
+    ActivityPub::ProcessAccountService.new.call(@username, @domain, @json)
   rescue Oj::ParseError
     nil
   end
 
   private
+
+  def parse_domain
+    URI.parse(@json['id']).host
+  end
 
   def supported_context?
     super(@json)
